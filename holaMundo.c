d2673eed9ae48612589e0ff2434d7df86f0ade52
Abel Farias Juarez <fari2207@gmail.com>
2015-04-05 18:19:33 -0300

    Commint message

---
+++ holaMundo.c
@@ -0,0 +1,14 @@
+/* Programa del tp # 0 hola mundo
+ *
+ * Abel Cesar, Farias Juarez
+ *
+ * 20150405
+ */
+
+#include <stdio.h> 
+
+int main(void){
+	puts("-_-_- HOLA MUNDO -_-_-\n");
+
+	//return EXIT_SUCCESS;
+}
